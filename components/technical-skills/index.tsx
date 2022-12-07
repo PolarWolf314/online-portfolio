32c8a2c73d8362e3ad53721bebf802737cdd38e4
PolarWolf314 <aaronjoshguo@gmail.com>
2022-12-08 11:13:26 +1300

    feat - finished styling the technical skills
    The technical skills and cards have been styled.

--- components/technical-skills/index.tsx
+++ components/technical-skills/index.tsx
@@ -1,15 +1,28 @@
 import TechnicalSkillsItemComponent from "./technical-skills-item-component";
+import bashLogo from "../../assets/logos/bash-logo.svg";
 
 const TechnicalSkills = () => {
     return (
         <>
-            <div>
-                <h1>Technical Skills</h1>
-                <div>
-                    <TechnicalSkillsItemComponent />
-                    <TechnicalSkillsItemComponent />
-                    <TechnicalSkillsItemComponent />
-                    <TechnicalSkillsItemComponent />
+            <h1>Technical Skills</h1>
+            <div className="flex flex-col gap-5">
+                <div
+                    /* Front End */ className="flex flex-col gap-2.5 items-center px-1.5 py-2.5 bg-gradient-to-br from-fuchsia-100 to-blue-100 rounded-md drop-shadow-lg"
+                >
+                    <h3 className="self-start">Front End</h3>
+                    <TechnicalSkillsItemComponent logo={bashLogo} name="Bash" />
+                    <TechnicalSkillsItemComponent logo={bashLogo} name="Bash" />
+                    <TechnicalSkillsItemComponent logo={bashLogo} name="Bash" />
+                    <TechnicalSkillsItemComponent logo={bashLogo} name="Bash" />
+                </div>
+                <div
+                    /* Front End */ className="flex flex-col gap-2.5 items-center px-1.5 py-2.5 bg-gradient-to-br from-fuchsia-100 to-blue-100 rounded-md drop-shadow-lg"
+                >
+                    <h3 className="self-start">Front End</h3>
+                    <TechnicalSkillsItemComponent logo={bashLogo} name="Bash" />
+                    <TechnicalSkillsItemComponent logo={bashLogo} name="Bash" />
+                    <TechnicalSkillsItemComponent logo={bashLogo} name="Bash" />
+                    <TechnicalSkillsItemComponent logo={bashLogo} name="Bash" />
                 </div>
             </div>
         </>
--- components/technical-skills/technical-skills-item-component.tsx
+++ components/technical-skills/technical-skills-item-component.tsx
@@ -1,9 +1,20 @@
-const TechnicalSkillsItemComponent = () => {
-    return ( 
-        <div>
-            I AM AN ITEM
+import Image from "next/image";
+
+const TechnicalSkillsItemComponent = ({
+    logo,
+    name,
+}: {
+    logo: any;
+    name: string;
+}) => {
+    return (
+        <div className="flex flex-row items-center gap-3 w-full px-2.5 py-1 bg-gradient-to-br from-fuchsia-50 to-blue-50 border rounded-xl drop-shadow">
+            <span>
+                <Image src={logo} alt={name} width={30} />
+            </span>
+            <span>{name}</span>
         </div>
-     );
-}
- 
+    );
+};
+
 export default TechnicalSkillsItemComponent;
