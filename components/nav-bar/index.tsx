a2f700eac6e09010f758223999a841c10e92b313
PolarWolf314 <aaronjoshguo@gmail.com>
2022-12-07 19:25:18 +1300

    feat - logic for opening and closing added
    The logic for opening and closing the menu using the burger menu button.

--- components/nav-bar/index.tsx
+++ components/nav-bar/index.tsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
+import NavBarMenuComponent from "./nav-bar-menu-component";
 
 const NavBar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const onClickHandler = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <div className="sticky z-10 top-0 flex flex-row justify-between px-4 py-2 bg-fuchsia-400 mb-3.5">
-            <span className="text-3xl font-bold">TITLE</span>
-            <span className="text-3xl self-center">
-                <AiOutlineMenu />
-            </span>
-        </div>
+        <>
+            <div
+                className={`sticky z-10 top-0 flex flex-row justify-between px-4 py-2 mb-3.5 transition duration-700 ease-in-out ${
+                    isOpen ? "bg-fuchsia-500" : "bg-fuchsia-400"
+                }`}
+            >
+                <span className="text-3xl font-bold">TITLE</span>
+                <span className="text-3xl self-center">
+                    <AiOutlineMenu onClick={onClickHandler} />
+                </span>
+            </div>
+            <div>
+                <NavBarMenuComponent isOpen={isOpen} />
+            </div>
+        </>
     );
 };
 
--- components/nav-bar/nav-bar-menu-component.tsx
+++ components/nav-bar/nav-bar-menu-component.tsx
@@ -1,8 +1,15 @@
 import Link from "next/link";
 
-const NavBarMenuComponent = () => {
+const NavBarMenuComponent = ({ isOpen }: { isOpen: boolean }) => {
     return (
-        <div>
+        <div
+            className={`fixed w-full top-[51px] overflow-hidden z-10
+            transition-all duration-700 ease-in-out ${
+                isOpen
+                    ? "bg-fuchsia-500 h-new-screen"
+                    : "bg-fuchsia-400 h-0 invisible"
+            }`}
+        >
             <div>
                 <Link href="/">ABOUT ME</Link>
             </div>
