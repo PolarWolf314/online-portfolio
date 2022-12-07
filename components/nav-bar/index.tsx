8b2087fe18b59ea7a15d072a2f9c072a72fa537d
PolarWolf314 <aaronjoshguo@gmail.com>
2022-12-07 18:28:22 +1300

    feat - styling for nav bar complete
    The nav bar's styling alone has been completed for the mobile view.

--- components/nav-bar/index.tsx
+++ components/nav-bar/index.tsx
@@ -2,9 +2,9 @@ import { AiOutlineMenu } from "react-icons/ai";
 
 const NavBar = () => {
     return (
-        <div>
-            <span>TITLE</span>
-            <span>
+        <div className="sticky z-10 top-0 flex flex-row justify-between px-4 py-2 bg-fuchsia-400 mb-3.5">
+            <span className="text-3xl font-bold">TITLE</span>
+            <span className="text-3xl self-center">
                 <AiOutlineMenu />
             </span>
         </div>
