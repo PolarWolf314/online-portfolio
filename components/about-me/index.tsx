76145ca145e121c07583eccbef23264f08dbd259
PolarWolf314 <aaronjoshguo@gmail.com>
2022-12-07 17:09:34 +1300

    feat - added a profile card component
    Basic text has been pasted in as well as the profile card component being made.

--- components/about-me/index.tsx
+++ components/about-me/index.tsx
@@ -1,8 +1,18 @@
+import ProfileCardComponent from "./profile-card-component";
+
 const AboutMe = () => {
     return (
         <div>
-            hello world
-            <div>This is the about me page</div>
+            <ProfileCardComponent />
+            <div>Hey</div>
+            <div>
+                I&apos;m Aaron, a 19 year old Software Engineering student at
+                the University of Auckland. I love developing robust,
+                consistent, and applicable software using modern frameworks such
+                as React. When I am not developing code, I will be spending time
+                with the people I love, playing video games, playing music, or
+                training.
+            </div>
         </div>
     );
 };
---
+++ components/about-me/profile-card-component.tsx
@@ -0,0 +1,18 @@
+const ProfileCardComponent = () => {
+    return ( 
+        <>
+            <div>
+                Profile Picture
+            </div>
+            <div>
+                Aaron Guo
+            </div>
+            <div>Second Year Software Engineering Student</div>
+            <div>
+                logos
+            </div>
+        </>
+     );
+}
+ 
+export default ProfileCardComponent;
