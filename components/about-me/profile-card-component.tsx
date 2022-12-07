79897efd89b6e7110db1a45573146104613a9c78
PolarWolf314 <aaronjoshguo@gmail.com>
2022-12-07 17:36:54 +1300

    feat - profile picture has been imported
    The profile picture for profile card component has been added in.

--- components/about-me/profile-card-component.tsx
+++ components/about-me/profile-card-component.tsx
@@ -1,15 +1,19 @@
+import Image from "next/image";
+
 import ProfileCardLogoComponent from "./profile-card-logo-component";
+
+const ProfilePicture = require("assets/images/profile-pic.jpg");
 
 const ProfileCardComponent = () => {
     return (
-        <>
-            <div>Profile Picture</div>
+        <div>
+            <Image src={ProfilePicture} alt="Profile Picture" />
             <div>Aaron Guo</div>
             <div>Second Year Software Engineering Student</div>
             <div>
                 <ProfileCardLogoComponent />
             </div>
-        </>
+        </div>
     );
 };
 
