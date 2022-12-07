70156dd6e300a96ede181a3daa14bb9b9f71f57f
PolarWolf314 <aaronjoshguo@gmail.com>
2022-12-08 12:17:20 +1300

    feat - added a project card component
    The initial setup for the project card component has been added.

--- components/projects/index.tsx
+++ components/projects/index.tsx
@@ -1,7 +1,10 @@
+import ProjectsCardComponent from "./projects-card-component";
+
 const Projects = () => {
     return ( 
         <div>
             <div>THIS IS THE PROJECTS COMPONENT</div>
+            <ProjectsCardComponent />
         </div>
      );
 }
---
+++ components/projects/projects-card-component.tsx
@@ -0,0 +1,9 @@
+const ProjectsCardComponent = () => {
+    return (
+        <div>
+            <div>THIS IS A PROJECT CARD COMPONENT</div>
+        </div>
+    );
+};
+
+export default ProjectsCardComponent;
