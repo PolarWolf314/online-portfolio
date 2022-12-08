f5fef134ea29e75d462faa74ff21246a101b6f06
PolarWolf314 <aaronjoshguo@gmail.com>
2022-12-08 13:13:56 +1300

    feat - finished styling project cards
    The basic styling for the project cards are complete.

--- components/projects/index.tsx
+++ components/projects/index.tsx
@@ -2,8 +2,8 @@ import ProjectsCardComponent from "./projects-card-component";
 
 const Projects = () => {
     return (
-        <div>
-            <div>THIS IS THE PROJECTS COMPONENT</div>
+        <div className="flex flex-col gap-4">
+            <h2>Projects</h2>
             <ProjectsCardComponent
                 title="React Websites"
                 date="Dec 2021"
@@ -16,7 +16,21 @@ const Projects = () => {
                         other projects.
                     </>
                 }
-                skills={<></>}
+                skills={<>React</>}
+            />
+            <ProjectsCardComponent
+                title="React Websites"
+                date="Dec 2021"
+                content={
+                    <>
+                        I completed 14 small React website projects as a part of
+                        my online learning. These websites challenged and grew
+                        my ability to create applications using React. Replace
+                        the 02 in the URL with integers up to 15 to see the
+                        other projects.
+                    </>
+                }
+                skills={<>React</>}
             />
         </div>
     );
--- components/projects/projects-card-component.tsx
+++ components/projects/projects-card-component.tsx
@@ -10,13 +10,15 @@ const ProjectsCardComponent = ({
     skills: any;
 }) => {
     return (
-        <div>
-            <div>
-                <span>{title}</span>
-                <span>{date}</span>
+        <div className="flex flex-col gap-y-3 bg-gradient-to-br from-fuchsia-100 to-blue-100 rounded-md drop-shadow-lg">
+            <div className="flex flex-row justify-between mt-2.5 px-1.5">
+                <h3 className="font-bold">{title}</h3>
+                <span className="text-slate-500">{date}</span>
             </div>
-            <div>{content}</div>
-            <div>{skills}</div>
+            <div className="text-justify bg-gradient-to-br from-fuchsia-50 to-blue-50 border-t border-slate-300 rounded-b-md px-4 pt-2 pb-2">
+                {content}
+                <div>{skills}</div>
+            </div>
         </div>
     );
 };
