f20cab68aec8c54feaab089ddae7a5f39e7d1945
PolarWolf314 <aaronjoshguo@gmail.com>
2022-12-10 19:49:04 +1300

    feat - finished styling technical skills
    Finished styling the technical skills section for desktop.

--- components/technical-skills/technical-skills-card-component.tsx
+++ components/technical-skills/technical-skills-card-component.tsx
@@ -1,15 +1,21 @@
 const TechnicalSkillsCardComponent = ({
     categoryName,
     items,
+    className,
 }: {
     categoryName: string;
     items: any;
+    className?: string;
 }) => {
     return (
         <div
-            /* Front End */ className="flex flex-col gap-2.5 items-center px-1.5 py-2.5 bg-gradient-to-br from-fuchsia-100 to-blue-100 rounded-md drop-shadow-lg"
+            className={`${
+                className ? className : ""
+            } flex flex-col gap-2.5 items-center px-1.5 py-2.5 bg-gradient-to-br from-fuchsia-100 to-blue-100 rounded-md drop-shadow-lg h-full w-full md:basis-almost-1/2`}
         >
-            <h3 className="self-start font-semibold">{categoryName}</h3>
+            <h3 className="self-start font-medium lg:text-xl 2xl:text-3xl">
+                {categoryName}
+            </h3>
             {items}
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,13 @@ module.exports = {
                 "new-screen": "calc(100vh - 51px)",
                 "new-screen-2": "calc(100vh - 101px)",
             },
+            flexBasis: {
+                "almost-1/2": "calc(50% - 0.625rem)",
+                "almost-1/3": "calc(33.333333% - 0.84rem)",
+            },
+            width: {
+                "almost-1/3": "calc(33.333333% - 0.84rem)",
+            },
         },
     },
     plugins: [],
